3a6ed18da0bbb7970f3087b6aee3da2df08da992
bl4ck-orig <sebho1996@gmail.com>
2023-11-27 18:09:27 +0100

    Initialize project

---
+++ ReinforcementLearning/Environment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReinforcementLearning
+{
+    public abstract class Environment
+    {
+        public List<int> ObservationSpace = new List<int>();
+        public List<int> ActionSpace = new List<int>();
+    }
+}
