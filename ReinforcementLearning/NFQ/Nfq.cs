9c0ef7ed3866dfdcd6995e2158b78f75cf952838
bl4ck-orig <sebho1996@gmail.com>
2024-01-13 14:10:19 +0100

    Clean up evaluation & better value clipping

---
+++ ReinforcementLearning/NFQ/Nfq.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Utilities;
+
+namespace ReinforcementLearning
+{
+    public class Nfq
+    {
+        private double learnRate;
+        private int batchSize;
+        private int epochs;
+        private double gamma;
+        private Environment<double[]> environment;
+        private int timeStepLimit;
+        private int seed;
+        private double maxMinutes;
+        private long maxEpisodes;
+        private IStrategy explorationStrategy;
+        private IStrategy trainingStrategy;
+
+        private Random prng;
+        private int nS;
+        private int nA;
+        private IFcq onlineModel;
+
+        private List<Experience<double[]>> experiences;
+        private List<double> episodeRewards;
+        private List<long> episodeTimeStep;
+        private List<long> episodeExploration;
+        private List<double> gradientMagnitudes;
+
+        public Nfq(NfqArgs _args)
+        {
+            learnRate = _args.LearnRate;
+            gamma = _args.Gamma;
+            batchSize = _args.BatchSize;
+            epochs = _args.Epochs;
+            environment = _args.Environment;
+            seed = _args.Seed;
+            maxMinutes = _args.MaxMinutes;
+            maxEpisodes = _args.MaxEpisodes;
+            explorationStrategy = _args.ExplorationStrategy;
+            trainingStrategy = _args.TrainingStrategy;
+            timeStepLimit = _args.TimeStepLimit;
+
+            prng = seed == -1 ? new Random() : new Random(seed);
+            nS = environment.ObservationSpaceSize;
+            nA = environment.ActionSpaceSize;
+
+            onlineModel = new NeuralNetwork(nS,
+                _args.HiddenLayerNodesAmount,
+                _args.HiddenLayersAmount,
+                nA,
+                batchSize,
+                _args.GradientClippingThreshold,
+                _args.MinZeroConvergeThreshold,
+                _args.FixNan,
+                _args.ClipValuesFirst,
+                prng);
+
+            experiences = new List<Experience<double[]>>();
+            episodeRewards = new List<double>();
+            episodeTimeStep = new List<long>();
+            episodeExploration = new List<long>();
+            gradientMagnitudes = new List<double>();
+        }
+
+        public Nfq(NfqArgs _args, NeuralNetwork _nn)
+        {
+            learnRate = _args.LearnRate;
+            gamma = _args.Gamma;
+            batchSize = _args.BatchSize;
+            epochs = _args.Epochs;
+            environment = _args.Environment;
+            seed = _args.Seed;
+            maxMinutes = _args.MaxMinutes;
+            maxEpisodes = _args.MaxEpisodes;
+            explorationStrategy = _args.ExplorationStrategy;
+            trainingStrategy = _args.TrainingStrategy;
+            timeStepLimit = _args.TimeStepLimit;
+
+            prng = seed == -1 ? new Random() : new Random(seed);
+            nS = environment.ObservationSpaceSize;
+            nA = environment.ActionSpaceSize;
+            onlineModel = _nn;
+
+            experiences = new List<Experience<double[]>>();
+            episodeRewards = new List<double>();
+            episodeTimeStep = new List<long>();
+            episodeExploration = new List<long>();
+            gradientMagnitudes = new List<double>();
+        }
+
+        public NfqResult Train()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            TimeSpan maxTimeSpan = TimeSpan.FromMinutes(maxMinutes);
+
+            bool isTrainingFinished = false;
+            string trainingFinishedReason = "";
+
+            for (int episode = 1; !isTrainingFinished; episode++)
+            {
+                double episodesFinishedPercent = (float)episode / maxEpisodes;
+                double timeFinishedPercent = stopwatch.Elapsed.TotalMilliseconds / maxTimeSpan.TotalMilliseconds;
+
+                Dialogue.PrintProgress((float)Math.Max(episodesFinishedPercent, timeFinishedPercent), episode == 1);
+
+                double[] state = environment.Reset(timeStepLimit != 0, true, prng, timeStepLimit);
+                bool isTerminal = false;
+                bool nanOccured = false;
+                episodeRewards.Add(0.0f);
+                episodeTimeStep.Add(0);
+                episodeExploration.Add(0);
+
+                for(long step = 0; !isTerminal ; step++)
+                {
+                    (double[] NextState, bool Done) stepResult = InteractionStep(state, onlineModel, environment);
+                    state = stepResult.NextState;
+                    isTerminal = stepResult.Done;
+
+                    if (InputManager.Interrupt)
+                        goto exit;
+
+                    if (experiences.Count < batchSize)
+                        continue;
+
+                    for (int i = 0; i < epochs; i++)
+                    {
+                        OptimizeModel();
+
+                        if (InputManager.Interrupt)
+                            goto exit;
+                    }
+
+                    experiences.Clear();
+                }
+
+            exit:
+                if (stopwatch.Elapsed >= maxTimeSpan)
+                {
+                    isTrainingFinished = true;
+                    trainingFinishedReason = "Max training time reached after " + maxTimeSpan.ToString() + ".";
+                }
+
+                if (nanOccured)
+                {
+                    isTrainingFinished = true;
+                    trainingFinishedReason = "Nan occured by learning rate of " + learnRate + " after " + stopwatch.Elapsed.ToString() + ".";
+                }
+
+                if (InputManager.Interrupt)
+                {
+                    isTrainingFinished = true;
+                    trainingFinishedReason = "Interrupt after " + stopwatch.Elapsed.ToString() + ".";
+                }
+
+                //if(episode >= maxEpisodes)
+                //{
+                //    isTrainingFinished = true;
+                //    trainingFinishedReason = "Max episodes reached after " + stopwatch.Elapsed.ToString() + ".";
+                //}
+            }
+
+            return new NfqResult(onlineModel,
+                learnRate,
+                trainingFinishedReason, 
+                episodeRewards,
+                episodeTimeStep, 
+                episodeExploration, 
+                gradientMagnitudes);
+        }
+
+        private (double[] NextState, bool Done) InteractionStep(double[] _state, IFcq _model, Environment<double[]> _environment)
+        {
+            int action = trainingStrategy.SelectAction(_state, _model, prng);
+            StepResult<double[]> stepResult = _environment.Step(action);
+            bool isFailure = stepResult.Done && !stepResult.IsTruncated;
+            Experience<double[]> experience = new Experience<double[]>(_state,
+                action,
+                stepResult.Reward,
+                stepResult.NextState,
+                isFailure ? 1.0f : 0.0f);
+
+            experiences.Add(experience);
+            episodeRewards[episodeRewards.Count - 1] += stepResult.Reward;
+            episodeTimeStep[episodeTimeStep.Count - 1] += 1;
+            episodeExploration[episodeExploration.Count - 1] += trainingStrategy.ExploratoryActionTaken ? 1 : 0;
+            return (stepResult.NextState, stepResult.Done || stepResult.IsTruncated);
+        }
+
+        private void OptimizeModel()
+        {
+            List<double[]> states = experiences.Select(x => x.State).ToList();
+            List<int> actions = experiences.Select(x => x.Action).ToList();
+            List<double> rewards = experiences.Select(x => x.Reward).ToList();
+            List<double[]> nextStates = experiences.Select(x => x.NextState).ToList();
+            List<double> isTerminals = experiences.Select(x => x.IsFailure).ToList();
+
+            double[,] nextStateFeatureMatrix = Commons.ToMatrix(nextStates).Transpose(); 
+            double[,] maxAQSp = onlineModel.GetOutputMatrix(nextStateFeatureMatrix);
+            double[] oneMinusTerminals = Commons.SubtractFromValue(1.0f, isTerminals).ToArray();
+            double[,] targetQS_a = Commons.MultiplyMatrixByArrayPerColumn(maxAQSp, oneMinusTerminals);
+            double[,] targetQS_b = Commons.Multiply(targetQS_a, gamma);
+            double[,] targetQS = Commons.AddVectorToMatrix(targetQS_b, rewards.ToArray());  
+
+            double[,] stateFeatureMatrix = Commons.ToMatrix(states).Transpose(); 
+            double[,] statePredictionOutput = onlineModel.GetOutputMatrix(stateFeatureMatrix);  
+            double[,] tdErrors = Commons.Subtract(targetQS, statePredictionOutput);
+
+            double[,] errorMatrix = new double[tdErrors.GetLength(0), tdErrors.GetLength(1)];
+            for (int i = 0; i < tdErrors.GetLength(0); i++)
+            {
+                for (int j = 0; j < tdErrors.GetLength(1); j++)
+                {
+                    errorMatrix[i, j] = Math.Pow(tdErrors[i, j], 2);
+                }
+            }
+
+            onlineModel.Backwards(errorMatrix);
+            double gradientMagnitude = onlineModel.AdjustWeightsAndBiases(learnRate);
+
+            try
+            {
+                gradientMagnitudes.Add(gradientMagnitude);
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ ReinforcementLearning/Program.cs
@@ -0,0 +1,235 @@
+﻿using ReinforcementLearning.Training;
+using ReinforcementLearning.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Utilities;
+
+namespace ReinforcementLearning
+{
+    internal class Program
+    {
+        private static string fileNameNoExt = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Model";
+        private static string fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\3_Model_0_2024_01_10-09_12.bin";
+        private static string fileNameReward = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Rewards.txt";
+
+        private static int batchSize = 1024;
+        private static double learnRate = 0.0001f;
+        private static double maxMinutes = 360f;
+        private static int timeStepLimit = 100;
+        private static double exploration = 0.15f;
+        private static double gradientClippingThreshold = 150f;
+        private static double minZeroConvergeThreshold = 0.00001f;
+        private static bool fixNan = false;
+        private static bool clipValuesFirst = false;
+
+        static void Main(string[] args)
+        {
+            //RunQLearning();
+            //ContinueNfq(fileName);
+            RunNfq();
+            //ExportRewardData(fileNameReward);
+            //TestModel(fileName);
+            //TestModelFull(fileName);
+        }
+
+        private static void RunQLearning()
+        {
+            QLearningResult qLearningResult = QLearning.Learn(new QLearningArgs(new EnvironemntFrozenLake()));
+
+            Console.WriteLine("\n" + qLearningResult.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void ContinueNfq(string _filename)
+        {
+            TrainingResult trainingResult = Serializer.DeserializeObject(_filename);
+
+            NeuralNetwork nn = new NeuralNetwork(trainingResult);
+
+            RunNfq(nn);
+        }
+
+        private static void RunNfq(NeuralNetwork _nn = null)
+        {
+            InputManager.ListenInputs();
+            
+            var initialStates = new List<Dictionary<EEnemyInput, double>>() 
+            { 
+                StartStates.initialStateStandard,
+                StartStates.initialStateLateGame,
+                StartStates.initialStateLateGameDefending,
+                StartStates.initialStateLateGameAttacking,
+                StartStates.initialStateMidGame,
+                StartStates.shouldTryDefend,
+                StartStates.shouldAttack,
+                StartStates.shouldEat,
+                StartStates.shouldTryBalanceTribes,
+            };
+
+            Random prng = new Random();
+            var environment = new EnvironmentRts(initialStates);
+
+            EGreedyStrategy trainingStrategy = new EGreedyStrategy(exploration);
+            GreedyStrategy evaluationStrategy = new GreedyStrategy();
+
+            NfqArgs nfqArgs = new NfqArgs(environment,
+                evaluationStrategy,
+                trainingStrategy,
+                _learnRate: learnRate,
+                _batchSize: batchSize,
+                _maxMinutes: maxMinutes,
+                _timeStepLimit: timeStepLimit,
+                _gradientClippingThreshold: gradientClippingThreshold,
+                _fixNan: fixNan,
+                _clipValuesFirst: clipValuesFirst,
+                _minZeroConvergeThreshold: minZeroConvergeThreshold);
+
+            Nfq nfq = _nn == null ? new Nfq(nfqArgs) : new Nfq(nfqArgs, _nn);
+
+            NfqResult result = nfq.Train();
+
+            string file = fileNameNoExt + "_0_" + DateTime.Now.ToString("yyyy_MM_dd-HH_mm") + ".bin";
+            for (int i = 0; File.Exists(file); i++)
+                file = fileNameNoExt + "_" + i + "_" + DateTime.Now.ToString("yyyy_MM_dd-HH_mm") + ".bin";
+
+            Serializer.SerializeObject(file, result.ToNeuralNetworkResults());
+
+            Console.WriteLine("\n" + result.EndReason + " Model saved as: " + file);
+            
+            Console.ReadKey();
+        }
+
+        private static void ExportRewardData(string _filename)
+        {
+            if (!File.Exists(_filename))
+                throw new ArgumentException("File name not existant");
+
+            TrainingResult _trainingResult = Serializer.DeserializeObject(_filename);
+
+            DataExporter.ExportData(_trainingResult.episodeRewards, _filename);
+        }
+
+        private static void TestModel(string _filename)
+        {
+            if (!File.Exists(_filename))
+                throw new ArgumentException("File name not existant");
+
+            TrainingResult trainingResult = Serializer.DeserializeObject(_filename);
+
+            NeuralNetwork nn = new NeuralNetwork(trainingResult);
+
+            Random prng = new Random();
+            GreedyStrategy greedy = new GreedyStrategy();
+
+            Console.WriteLine("Should defend: " + (EEnemyOperation)greedy.SelectAction(StartStates.shouldTryDefend.Values.ToArray(), nn, prng));
+            Console.WriteLine("Should attack: " + (EEnemyOperation)greedy.SelectAction(StartStates.shouldAttack.Values.ToArray(), nn, prng));
+            Console.WriteLine("Should eat: " + (EEnemyOperation)greedy.SelectAction(StartStates.shouldEat.Values.ToArray(), nn, prng));
+            Console.WriteLine("Should balance tribes: " + (EEnemyOperation)greedy.SelectAction(StartStates.shouldTryBalanceTribes.Values.ToArray(), nn, prng));
+
+            Console.ReadKey();
+        }
+
+
+        private static void TestModelFull(string _filename)
+        {
+            int i = 1;
+
+            Console.WriteLine(_filename + "\n\n");
+            Console.WriteLine("Standard");
+            TestState(_filename, i++, StartStates.initialStateStandard);
+            Console.WriteLine("Late Game");
+            TestState(_filename, i++, StartStates.initialStateLateGame);
+            Console.WriteLine("Late Game Defending");
+            TestState(_filename, i++, StartStates.initialStateLateGameDefending);
+            Console.WriteLine("Late Game Attacking");
+            TestState(_filename, i++, StartStates.initialStateLateGameAttacking);
+            Console.WriteLine("Mid Game");
+            TestState(_filename, i++, StartStates.initialStateMidGame);
+            Console.WriteLine("Should Defend");
+            TestState(_filename, i++, StartStates.shouldTryDefend);
+            Console.WriteLine("Should Attack");
+            TestState(_filename, i++, StartStates.shouldAttack);
+            Console.WriteLine("Should Eat");
+            TestState(_filename, i++, StartStates.shouldEat);
+            Console.WriteLine("Should balance tribes");
+            TestState(_filename, i++, StartStates.shouldTryBalanceTribes);
+
+            Console.ReadKey();
+        }
+
+        private static void TestState(string _filename, int _seed, Dictionary<EEnemyInput, double> _state)
+        {
+            var testModelResult = TestModelState(_filename, _seed, _state);
+            var dummyResult = TestModelStateDummy(_seed, _state);
+            Console.WriteLine("[DUMMY] Cumulative Reward: " + dummyResult.CumulativeReward + "\t Final Reward: " + dummyResult.LastReward);
+            Console.WriteLine("[TEST]  Cumulative Reward: " + testModelResult.CumulativeReward + "\t Final Reward: " + testModelResult.LastReward);
+        }
+
+
+        private static (double CumulativeReward, double LastReward) TestModelState(string _filename, int _seed, Dictionary<EEnemyInput, double> _state)
+        {
+            if (!File.Exists(_filename))
+                throw new ArgumentException("File name not existant");
+
+            TrainingResult trainingResult = Serializer.DeserializeObject(_filename);
+
+            NeuralNetwork nn = new NeuralNetwork(trainingResult);
+
+            Random prngTest = new Random();
+            GreedyStrategy greedy = new GreedyStrategy();
+
+            EnvironmentRts env = new EnvironmentRts(new List<Dictionary<EEnemyInput, double>>() { _state });
+
+            env.Reset(true, true, _seed, timeStepLimit);
+
+            double cumulativeReward = 0f;
+            bool done = false;
+
+            StepResult<double[]> result = default;
+
+            while (!done)
+            {
+                var action = greedy.SelectAction(env.State, nn, prngTest);
+                result = env.Step(action);
+
+                //Console.WriteLine((EEnemyOperation)action);
+
+                cumulativeReward += result.Reward;
+
+                done = result.IsTruncated || result.Done;
+            }
+
+            return (cumulativeReward, result.Reward);
+        }
+
+        private static (double CumulativeReward, double LastReward) TestModelStateDummy(int _seed, Dictionary<EEnemyInput, double> _state)
+        {
+            Random prngTest = new Random();
+            GreedyStrategy greedy = new GreedyStrategy();
+
+            EnvironmentRts env = new EnvironmentRts(new List<Dictionary<EEnemyInput, double>>() { _state });
+
+            env.Reset(true, true, _seed, timeStepLimit);
+
+            double cumulativeReward = 0f;
+            bool done = false;
+
+            StepResult<double[]> result = default;
+
+            while (!done)
+            {
+                result = env.Step((int)EEnemyOperation.None);
+
+                cumulativeReward += result.Reward;
+
+                done = result.IsTruncated || result.Done;
+            }
+
+            return (cumulativeReward, result.Reward);
+        }
+
+    }
+}
